0f6af0c16ff938fc1d2654d7967e927b82489753
ReblochonMasque <trustgold@gmail.com>
2021-01-01 14:58:47 +0800

    add tests for from_iterable, __str__, __len__, __bool__, __reversed__

--- congeries/tests/test_dlist.py
+++ congeries/tests/test_dlist.py
@@ -1,6 +1,8 @@
+import io
 import unittest
 
 from congeries.src import dlist
+from contextlib import redirect_stdout
 
 
 class Test_dlist(unittest.TestCase):
@@ -11,10 +13,53 @@ class Test_dlist(unittest.TestCase):
     def test_len_0(self):
         self.assertEqual(len(dlist()), 0)
 
-    # todo: test non empty len and truthfulness
+    def test_from_iterable_empty(self):
+        dl = dlist.from_iterable([])
+        self.assertEqual(len(dl), 0)
 
-    def test_empty_container(self):
-        self.assertFalse(dlist(), False)
+    def test_from_iterable_1(self):
+        dl = dlist.from_iterable([1])
+        self.assertEqual(len(dl), 1)
+
+    def test_from_iterable_2(self):
+        dl = dlist.from_iterable([1, 2, 5, 8, 9])
+        self.assertEqual(len(dl), 5)
+
+    def test_truthfulness_empty_container(self):
+        dl = dlist.from_iterable([])
+        self.assertFalse(dl)
+
+    def test_truthfulness_1(self):
+        dl = dlist.from_iterable([-1])
+        self.assertTrue(dl)
+
+    def test_truthfulness_2(self):
+        dl = dlist.from_iterable([1, 2, 5, 8, 9])
+        self.assertTrue(dl)
+
+    def test_str_empty(self):
+        dl = dlist.from_iterable([])
+        actual = io.StringIO()
+        with redirect_stdout(actual):
+            print(dl, end='')
+        expected = 'dlist()'
+        self.assertEqual(actual.getvalue(), expected)
+
+    def test_str_non_empty(self):
+        dl = dlist.from_iterable([1, 2, 3])
+        actual = io.StringIO()
+        with redirect_stdout(actual):
+            print(dl, end='')
+        expected = 'dlist(1 <-> 2 <-> 3)'
+        self.assertEqual(actual.getvalue(), expected)
+
+    def test_reversed_via_str_non_empty(self):
+        dl = dlist.from_iterable(range(4))
+        actual = io.StringIO()
+        with redirect_stdout(actual):
+            print(dlist.from_iterable(reversed(dl)), end='')
+        expected = 'dlist(3 <-> 2 <-> 1 <-> 0)'
+        self.assertEqual(actual.getvalue(), expected)
 
 
 if __name__ == '__main__':
