e4ccb090d3eab04e0852c611987a779ac6feb3f6
ReblochonMasque <trustgold@gmail.com>
2021-01-01 09:09:41 +0800

    Add add inner class Record

--- congeries/src/linkedlists.py
+++ congeries/src/linkedlists.py
@@ -1,13 +1,39 @@
 """
 a module with singly and doubly linked lists
+
+slist a singly linked list
+    create: slist() or slist(iterable)
+dlist a doubly linked list
+    create: dlist() or dlist(iterable)
+clist a circular (doubly) linked list
+    create: clist() or clist(iterable)
+
 """
+
+from typing import Iterable
 
 
 class dlist:
     """a Doubly Linked List representation
     """
 
+    class Record:
+        """
+        represents a node that carries a payload (data), and links
+        to the previous and next records in the line
+        """
+        def __init__(
+                self,
+                payload=None,
+                prev: 'dlist.Record' = None,
+                suiv: 'dlist.Record' = None,
+        ) -> None:
+            self.payload = payload
+            self.prev = prev
+            self.suiv = suiv
 
+        def __str__(self, it: Iterable = None) -> None:
+            return str(self.payload)
 
 
 if __name__ == '__main__':
