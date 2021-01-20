63807d5391f5aaa7d145a259691a89f3dfee16b7
ReblochonMasque <trustgold@gmail.com>
2021-01-20 12:15:39 +0800

    add __eq__ and __ne__

--- congeries/src/positionallist.py
+++ congeries/src/positionallist.py
@@ -42,7 +42,25 @@ class PositionalList(DoublyLinkedList):
             """
             return self._record.payload
 
+        def __eq__(self, other: 'PositionalList.Position') -> bool:
+            """compares for equality as in representing the same location
 
+            Several Position objects may be created to represent the same record;
+            they are considered equal.
+            :other: a PositionalList.Position object
+            :return: True if self represents the same record as other
+            """
+            return type(other) is type(self) and other._record is self._record
+
+        def __ne__(self, other) -> bool:
+            """compares for inequality as in not representing the same location
+
+            Several Position objects may be created to represent the same record;
+            they are considered equal.
+            :other: a PositionalList.Position object
+            :return: True if self represents the same record as other
+            """
+            return not (self == other)
 
 
 if __name__ == '__main__':
