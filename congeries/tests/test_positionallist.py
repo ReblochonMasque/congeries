4dcacaf66b6833d7dc72342118a9eb8984172ab1
ReblochonMasque <trustgold@gmail.com>
2021-01-20 14:39:56 +0800

    add tests to add_first

--- congeries/tests/test_positionallist.py
+++ congeries/tests/test_positionallist.py
@@ -8,10 +8,23 @@ class TestPositionalList(unittest.TestCase):
     def test_type(self):
         self.assertIsInstance(PositionalList(), PositionalList)
 
-    def test_add_first(self):
+    def test_add_first_1(self):
         pl = PositionalList()
         pl.add_first('a')
         self.assertEqual(len(pl), 1)
+
+    def test_add_first_2(self):
+        pl = PositionalList()
+        pl.add_first('b')
+        pl.add_first('a')
+        self.assertEqual(len(pl), 2)
+
+    def test_add_first_3(self):
+        pl = PositionalList()
+        pl.add_first('c')
+        pl.add_first('b')
+        pl.add_first('a')
+        self.assertEqual(len(pl), 3)
 
 
 if __name__ == '__main__':
