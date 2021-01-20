66910965169840383b87867c948ebdd04d7f08b7
ReblochonMasque <trustgold@gmail.com>
2021-01-20 08:43:25 +0800

    add module and class docstrings

--- congeries/src/positionallist.py
+++ congeries/src/positionallist.py
@@ -1,5 +1,6 @@
 """
-
+general abstraction of a sequence of elements with the ability
+to identify the location of an element
 """
 
 from typing import Iterator
@@ -8,7 +9,13 @@ from congeries.src.linkedlistsbases import DLLBase
 
 class PositionalList(DLLBase):
     """
+    implementation of a PositionalList class using a doubly linked list that
+    satisﬁes the requirement that each method of the positional list ADT runs
+    in worst-case O(1) time when implemented with a doubly linked list.
 
+    We rely on DLLBase class for our low-level representation.
+    The primary responsibility of PositionalList is to provide a public interface
+    in accordance with the positional list ADT
     """
     def __iter__(self) -> Iterator:
         pass
