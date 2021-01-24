56ac8b4070788f7419d2d62477d50984a8de6f04
ReblochonMasque <trustgold@gmail.com>
2021-01-25 05:31:41 +0800

    stub FileDict

---
+++ congeries/src/filedict.py
@@ -0,0 +1,26 @@
+"""
+
+Regular dictionaries are implemented with in-memory hash tables. They are not persistent or
+shareable between processes. Also, they cannot be inspected externally while a program is
+running
+
+Files in a directory have none of these limitations.
+
+from Raymond Hettinger pycon.ru 2019
+Build powerful new data structures with Python's abstract base classes
+
+
+How to detect if any element in a dictionary changes?
+https://stackoverflow.com/questions/26189090/how-to-detect-if-any-element-in-a-dictionary-changes
+
+
+
+"""
+
+
+class FileDict:
+    pass
+
+
+if __name__ == '__main__':
+    pass
