bd8f4529178c1dcf37f30be407611d1411443cc1
ReblochonMasque <trustgold@gmail.com>
2021-01-25 05:21:45 +0800

    add Test_FileDict class

---
+++ congeries/tests/test_filedict.py
@@ -0,0 +1,13 @@
+# import io
+import unittest
+
+# from congeries.src import FileDict
+# from contextlib import redirect_stdout
+
+
+class Test_FileDict(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
