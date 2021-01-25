9360c6cec7f880ccf826c0bce0e31dcef60fb005
ReblochonMasque <trustgold@gmail.com>
2021-01-26 07:06:32 +0800

    add rename Test_FileDict to TestFileDict

--- congeries/tests/test_filedict.py
+++ congeries/tests/test_filedict.py
@@ -5,7 +5,7 @@ from congeries.src import FileDict
 # from contextlib import redirect_stdout
 
 
-class Test_FileDict(unittest.TestCase):
+class TestFileDict(unittest.TestCase):
 
     def test_type(self):
         self.assertIsInstance(FileDict(), FileDict)
