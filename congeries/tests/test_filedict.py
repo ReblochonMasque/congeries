fefa20ef1b095eda2c0a82cfea5bc23f847d4b86
ReblochonMasque <trustgold@gmail.com>
2021-01-26 09:43:25 +0800

    add docstring and comments to test_type_in_non_existent_directory

--- congeries/tests/test_filedict.py
+++ congeries/tests/test_filedict.py
@@ -10,13 +10,22 @@ from congeries.src import FileDict
 class TestFileDict(unittest.TestCase):
 
     def test_type_in_non_existent_directory(self):
+        """
+        creates a FileDict in a new directory
+        making sure that the directory does not pre-exist, and
+        removes it afterwards
+        """
         path = os.getcwd()
         dirname = 'temptestfiledict'
         fulldirpath = os.path.join(path, dirname)
+        # making sure that the directory does not pre-exist
         self.assertFalse(os.path.isdir(fulldirpath))
+        # this creates the directory
         self.assertIsInstance(FileDict('temptestfiledict'), FileDict)
+        # asserting the directory was created
         self.assertTrue(os.path.isdir(fulldirpath))
         os.rmdir(fulldirpath)
+        # verifies the directory was successfully removed
         self.assertFalse(os.path.isdir(fulldirpath))
 
     def test_type(self):
