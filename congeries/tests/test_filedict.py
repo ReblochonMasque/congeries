7cc89ec8891262bd64227e0ecf6f6a62f2c4a888
ReblochonMasque <trustgold@gmail.com>
2021-01-26 09:51:40 +0800

    add test_type_in_non_existent_directory_and_suppression_of_FileExistsError

--- congeries/tests/test_filedict.py
+++ congeries/tests/test_filedict.py
@@ -24,6 +24,34 @@ class TestFileDict(unittest.TestCase):
         self.assertIsInstance(FileDict('temptestfiledict'), FileDict)
         # asserting the directory was created
         self.assertTrue(os.path.isdir(fulldirpath))
+
+        os.rmdir(fulldirpath)
+        # verifies the directory was successfully removed
+        self.assertFalse(os.path.isdir(fulldirpath))
+
+    def test_type_in_non_existent_directory_and_suppression_of_FileExistsError(self):
+        """
+        creates a FileDict in a new directory
+        making sure that the directory does not pre-exist,
+        re-creates that dictionary, verifying the suppression
+        of FileExistsError
+        removes the directory afterwards
+        """
+        path = os.getcwd()
+        dirname = 'temptestfiledict'
+        fulldirpath = os.path.join(path, dirname)
+        # making sure that the directory does not pre-exist
+        self.assertFalse(os.path.isdir(fulldirpath))
+        # this creates the directory
+        self.assertIsInstance(FileDict('temptestfiledict'), FileDict)
+        # asserting the directory was created
+        self.assertTrue(os.path.isdir(fulldirpath))
+
+        # re-creating a Filedict in that directory
+        self.assertIsInstance(FileDict('temptestfiledict'), FileDict)
+        # asserting the directory still exists
+        self.assertTrue(os.path.isdir(fulldirpath))
+
         os.rmdir(fulldirpath)
         # verifies the directory was successfully removed
         self.assertFalse(os.path.isdir(fulldirpath))
