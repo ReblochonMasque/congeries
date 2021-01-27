f7c8da3ba8bd594345917506687cfa0910be9712
ReblochonMasque <trustgold@gmail.com>
2021-01-27 13:50:47 +0800

    add _extract_key helper

--- congeries/tests/test_filedotdict.py
+++ congeries/tests/test_filedotdict.py
@@ -2,7 +2,7 @@
 test suite for FileDotDict
 
 """
-
+import os
 import shutil
 import tempfile
 import unittest
@@ -19,6 +19,13 @@ class TestFileDotDict(unittest.TestCase):
 
     def tearDown(self) -> None:
         shutil.rmtree(self.tempdirname)
+
+    def _make_path(self, key):
+        pass
+
+    def _extract_key(self, tmpkey):
+        """remove path and dot"""
+        return tmpkey.name.split('/')[-1][1:]
 
     def test_type(self):
         self.assertIsInstance(self.fdd, FileDotDict)
@@ -42,9 +49,8 @@ class TestFileDotDict(unittest.TestCase):
             delete=False,
         ) as tmpkey:
             tmpkey.write('bob was here')
-            print(f'{tmpkey.name=}')
 
-        key = tmpkey.name.split('/')[-1][1:]  # remove path and dot
+        key = self._extract_key(tmpkey)
 
         actual = self.fdd[key]
         expected = 'bob was here'
