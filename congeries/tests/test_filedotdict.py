8cc5daafa40b46594d9480644eb4f07b6308434b
ReblochonMasque <trustgold@gmail.com>
2021-01-27 13:45:58 +0800

    add test_getitem_yes

--- congeries/tests/test_filedotdict.py
+++ congeries/tests/test_filedotdict.py
@@ -27,6 +27,29 @@ class TestFileDotDict(unittest.TestCase):
         with self.assertRaises(KeyError):
             self.fdd['key is not there']
 
+    def test_getitem_yes(self):
+        """
+        uses a FileDotDict in a temp directory
+        creates a temp file in that directory
+        retrieves the file content using its name as key
+        """
+        with tempfile.NamedTemporaryFile(
+            mode='w+t',
+            buffering=-1,
+            prefix='.',
+            encoding=None,
+            dir=self.tempdirname,
+            delete=False,
+        ) as tmpkey:
+            tmpkey.write('bob was here')
+            print(f'{tmpkey.name=}')
+
+        key = tmpkey.name.split('/')[-1][1:]  # remove path and dot
+
+        actual = self.fdd[key]
+        expected = 'bob was here'
+        self.assertEqual(expected, actual)
+
     # def test_assign(self):
     #     self.fdd['abc'] = 'def'
 
