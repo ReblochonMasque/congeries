4b95567100efae3eef5e4c38e4a45b9c79f380da
ReblochonMasque <trustgold@gmail.com>
2021-01-27 15:42:09 +0800

    refactor test_filedict.py from parametrization of test_filedotdict.py

--- congeries/tests/test_filedict.py
+++ congeries/tests/test_filedict.py
@@ -1,5 +1,11 @@
+"""
+test suite for FileDict
+
+"""
+
 import io
 import os
+import shutil
 import tempfile
 import unittest
 
@@ -8,99 +14,108 @@ from contextlib import redirect_stdout
 
 
 class TestFileDict(unittest.TestCase):
-    """
-    conducts tests in temporary directories created with the
-    imported module tempfile.
-    Creation of directories and files, and their clean up are
-    automated
-    """
+
+    prefix = ''
 
     def setUp(self) -> None:
         self.tmpdirtest = tempfile.TemporaryDirectory()
-        self.fd = FileDict(self.tmpdirtest.name)
+        self.fdd = FileDict(self.tmpdirtest.name)
+        self.tempdirname = self.prefix + self.tmpdirtest.name
 
     def tearDown(self) -> None:
         self.tmpdirtest.cleanup()
 
+    def _make_path(self, key):
+        """helper to add dot to fullkey"""
+        return self.tempdirname + '/' + self.prefix + str(key)
+
+    def _extract_key(self, tmpkey):
+        """helper to remove path and dot"""
+        return tmpkey.name.split('/')[-1][len(self.prefix):]
+
     def test_type(self):
-        self.assertIsInstance(self.fd, FileDict)
+        self.assertIsInstance(self.fdd, FileDict)
 
     def test_getitem_None(self):
         with self.assertRaises(KeyError):
-            self.fd['key is not there']
+            self.fdd['key is not there']
 
     def test_getitem_yes(self):
         """
-        uses a FileDict in a temp directory
+        uses a FileDotDict in a temp directory
         creates a temp file in that directory
         retrieves the file content using its name as key
         """
         with tempfile.NamedTemporaryFile(
             mode='w+t',
             buffering=-1,
+            prefix=self.prefix,
             encoding=None,
-            dir=self.tmpdirtest.name,
+            dir=self.tempdirname,
             delete=False,
         ) as tmpkey:
             tmpkey.write('bob was here')
 
-        key = tmpkey.name.split('/')[-1]  # remove path
+        key = self._extract_key(tmpkey)
 
-        actual = self.fd[key]
+        actual = self.fdd[key]
         expected = 'bob was here'
         self.assertEqual(expected, actual)
 
     def test_setitem_0(self):
         key, value = 'abc', 'this is a new file'
-        self.fd[key] = value
-        self.assertTrue(os.path.exists(self.tmpdirtest.name + '/' + key))
-        with open(self.tmpdirtest.name + '/' + key, 'r') as f:
+        self.fdd[key] = value
+        fullkey = self._make_path(key)
+        self.assertTrue(os.path.exists(fullkey))
+        with open(fullkey, 'r') as f:
             actual = f.read()
         expected = value
         self.assertEqual(expected, actual)
 
     def test_setitem_num_key(self):
         key, value = 1, 'this is a new file'
-        self.fd[key] = value
-        strkey = str(key)   # <- see _get_fullname() is casting key to str
-        self.assertTrue(os.path.exists(self.tmpdirtest.name + '/' + strkey))
-        with open(self.tmpdirtest.name + '/' + strkey, 'r') as f:
+        self.fdd[key] = value
+        fullkey = self._make_path(key)
+        self.assertTrue(os.path.exists(fullkey))
+        with open(fullkey, 'r') as f:
             actual = f.read()
         expected = value
         self.assertEqual(expected, actual)
 
     def test_setitem_num_value(self):
         key, value = 'abc', 1234
-        self.fd[key] = value
-        self.assertTrue(os.path.exists(self.tmpdirtest.name + '/' + key))
-        with open(self.tmpdirtest.name + '/' + key, 'r') as f:
+        self.fdd[key] = value
+        fullkey = self._make_path(key)
+        self.assertTrue(os.path.exists(fullkey))
+        with open(fullkey, 'r') as f:
             actual = f.read()
         expected = str(value)   # <- see __setitem__() is casting value to str
         self.assertEqual(expected, actual)
 
     def test_setitem_numkey_numvalue(self):
         key, value = 1, 1234
-        self.fd[key] = value
-        strkey = str(key)
-        self.assertTrue(os.path.exists(self.tmpdirtest.name + '/' + strkey))
-        with open(self.tmpdirtest.name + '/' + strkey, 'r') as f:
+        self.fdd[key] = value
+        fullkey = self._make_path(key)
+        self.assertTrue(os.path.exists(fullkey))
+        with open(fullkey, 'r') as f:
             actual = f.read()
         expected = str(value)   # <- see __setitem__() is casting value to str
         self.assertEqual(expected, actual)
 
     def test_setitem_overwrite(self):
         key, value = 'abc', 'this is the original value'
-        self.fd[key] = value
-        self.assertTrue(os.path.exists(self.tmpdirtest.name + '/' + key))
-        with open(self.tmpdirtest.name + '/' + key, 'r') as f:
+        self.fdd[key] = value
+        fullkey = self._make_path(key)
+        self.assertTrue(os.path.exists(fullkey))
+        with open(fullkey, 'r') as f:
             actual = f.read()
         expected = value
         self.assertEqual(expected, actual)
 
         new_value = "this is the new value"
-        self.fd[key] = new_value
-        self.assertTrue(os.path.exists(self.tmpdirtest.name + '/' + key))
-        with open(self.tmpdirtest.name + '/' + key, 'r') as f:
+        self.fdd[key] = new_value
+        self.assertTrue(os.path.exists(fullkey))
+        with open(fullkey, 'r') as f:
             actual = f.read()
         expected = new_value
         self.assertEqual(expected, actual)
@@ -112,20 +127,23 @@ class TestFileDict(unittest.TestCase):
         deletes the file
         """
         with tempfile.NamedTemporaryFile(
-                mode='w+t',
-                buffering=-1,
-                encoding=None,
-                dir=self.tmpdirtest.name,
-                delete=False,
+            mode='w+t',
+            buffering=-1,
+            encoding=None,
+            prefix=self.prefix,
+            dir=self.tempdirname,
+            delete=False,
         ) as tmpkey:
             tmpkey.write('bob was here')
 
-        key = tmpkey.name.split('/')[-1]  # remove path
-        self.assertTrue(os.path.exists(self.tmpdirtest.name + '/' + key))
-        self.assertIn(key, os.listdir(self.tmpdirtest.name))
-        del self.fd[key]
-        self.assertFalse(os.path.exists(self.tmpdirtest.name + '/' + key))
-        self.assertNotIn(key, os.listdir(self.tmpdirtest.name))
+        key = self._extract_key(tmpkey)
+        fullkey = self._make_path(key)
+        prefixed_key = self.prefix + key
+        self.assertTrue(os.path.exists(fullkey))
+        self.assertIn(prefixed_key, os.listdir(self.tempdirname))
+        del self.fdd[key]
+        self.assertFalse(os.path.exists(fullkey))
+        self.assertNotIn(prefixed_key, os.listdir(self.tempdirname))
 
     def test_delitem_1(self):
         """
@@ -134,44 +152,50 @@ class TestFileDict(unittest.TestCase):
         deletes the two files
         """
         with tempfile.NamedTemporaryFile(
-                mode='w+t',
-                buffering=-1,
-                encoding=None,
-                dir=self.tmpdirtest.name,
-                delete=False,
+            mode='w+t',
+            buffering=-1,
+            encoding=None,
+            prefix=self.prefix,
+            dir=self.tempdirname,
+            delete=False,
         ) as tmpkey0:
             tmpkey0.write('bob was here')
 
         with tempfile.NamedTemporaryFile(
-                mode='w+t',
-                buffering=-1,
-                encoding=None,
-                dir=self.tmpdirtest.name,
-                delete=False,
+            mode='w+t',
+            buffering=-1,
+            encoding=None,
+            prefix=self.prefix,
+            dir=self.tempdirname,
+            delete=False,
         ) as tmpkey1:
             tmpkey1.write('bob was also here')
 
-        key0 = tmpkey0.name.split('/')[-1]  # remove path
-        self.assertTrue(os.path.exists(self.tmpdirtest.name + '/' + key0))
-        self.assertIn(key0, os.listdir(self.tmpdirtest.name))
+        key0 = self._extract_key(tmpkey0)
+        fullkey0 = self._make_path(key0)
+        prefixed_key0 = self.prefix + key0
+        self.assertTrue(os.path.exists(fullkey0))
+        self.assertIn(prefixed_key0, os.listdir(self.tempdirname))
 
-        key1 = tmpkey1.name.split('/')[-1]  # remove path
-        self.assertTrue(os.path.exists(self.tmpdirtest.name + '/' + key1))
-        self.assertIn(key1, os.listdir(self.tmpdirtest.name))
+        key1 = self._extract_key(tmpkey1)
+        fullkey1 = self._make_path(key1)
+        prefixed_key1 = self.prefix + key1
+        self.assertTrue(os.path.exists(fullkey1))
+        self.assertIn(prefixed_key1, os.listdir(self.tempdirname))
 
-        del self.fd[key1]
-        self.assertFalse(os.path.exists(self.tmpdirtest.name + '/' + key1))
-        self.assertNotIn(key1, os.listdir(self.tmpdirtest.name))
+        del self.fdd[key1]
+        self.assertFalse(os.path.exists(fullkey1))
+        self.assertNotIn(prefixed_key1, os.listdir(self.tempdirname))
 
-        del self.fd[key0]
-        self.assertFalse(os.path.exists(self.tmpdirtest.name + '/' + key0))
-        self.assertNotIn(key0, os.listdir(self.tmpdirtest.name))
+        del self.fdd[key0]
+        self.assertFalse(os.path.exists(fullkey0))
+        self.assertNotIn(prefixed_key0, os.listdir(self.tempdirname))
 
     def test_len_0(self):
         """
         checks the length of an empty FileDict
         """
-        self.assertEqual(len(self.fd), 0)
+        self.assertEqual(len(self.fdd), 0)
 
     def test_len_1_a(self):
         """
@@ -179,8 +203,8 @@ class TestFileDict(unittest.TestCase):
         adds a value to dict
         check the length
         """
-        self.fd['abc'] = "joe le taxi"
-        self.assertEqual(len(self.fd), 1)
+        self.fdd['abc'] = "joe le taxi"
+        self.assertEqual(len(self.fdd), 1)
 
     def test_len_1(self):
         """
@@ -189,15 +213,16 @@ class TestFileDict(unittest.TestCase):
         check the length
        """
         with tempfile.NamedTemporaryFile(
-                mode='w+t',
-                buffering=-1,
-                encoding=None,
-                dir=self.tmpdirtest.name,
-                delete=False,
+            mode='w+t',
+            buffering=-1,
+            encoding=None,
+            prefix=self.prefix,
+            dir=self.tempdirname,
+            delete=False,
         ) as tmpkey:
             tmpkey.write('bob was here')
 
-        self.assertEqual(len(self.fd), 1)
+        self.assertEqual(len(self.fdd), 1)
 
     def test_len_2_a(self):
         """
@@ -205,9 +230,9 @@ class TestFileDict(unittest.TestCase):
         adds two values to the dict
         check the length
         """
-        self.fd['abc'] = "joe le taxi"
-        self.fd['def'] = "les sucettes a l'anis"
-        self.assertEqual(len(self.fd), 2)
+        self.fdd['abc'] = "joe le taxi"
+        self.fdd['def'] = "les sucettes a l'anis"
+        self.assertEqual(len(self.fdd), 2)
 
     def test_len_2(self):
         """
@@ -216,24 +241,26 @@ class TestFileDict(unittest.TestCase):
         check the length
         """
         with tempfile.NamedTemporaryFile(
-                mode='w+t',
-                buffering=-1,
-                encoding=None,
-                dir=self.tmpdirtest.name,
-                delete=False,
+            mode='w+t',
+            buffering=-1,
+            encoding=None,
+            prefix=self.prefix,
+            dir=self.tempdirname,
+            delete=False,
         ) as tmpkey0:
             tmpkey0.write('bob was here')
 
         with tempfile.NamedTemporaryFile(
-                mode='w+t',
-                buffering=-1,
-                encoding=None,
-                dir=self.tmpdirtest.name,
-                delete=False,
+            mode='w+t',
+            buffering=-1,
+            encoding=None,
+            prefix=self.prefix,
+            dir=self.tempdirname,
+            delete=False,
         ) as tmpkey1:
             tmpkey1.write('bob was also here')
 
-        self.assertEqual(len(self.fd), 2)
+        self.assertEqual(len(self.fdd), 2)
 
     def test_delitem_no_key(self):
         """
@@ -243,44 +270,44 @@ class TestFileDict(unittest.TestCase):
         """
         key = 'abc'
         with self.assertRaises(KeyError):
-            del self.fd[key]
+            del self.fdd[key]
 
     def test_iter_empty(self):
-        it = iter(self.fd)
+        it = iter(self.fdd)
         with self.assertRaises(StopIteration):
             next(it)
 
     def test_iter_3(self):
         data = [('a', 'key is a'), ('b', 'key is b'), ('c', 'key is c'), ('wwwf', 'that hurts')]
         for k, v in data:
-            self.fd[k] = v
+            self.fdd[k] = v
 
-        for (k, v), (dk, dv) in zip(sorted(self.fd.items()), sorted(data)):
+        for (k, v), (dk, dv) in zip(sorted(self.fdd.items()), sorted(data)):
             self.assertEqual(k, dk)
             self.assertEqual(v, dv)
 
     def test_str_empty(self):
         actual = io.StringIO()
         with redirect_stdout(actual):
-            print(self.fd, end='')
+            print(self.fdd, end='')
         expected = "FileDict()"
         self.assertEqual(actual.getvalue(), expected)
 
     def test_repr_empty(self):
         actual = io.StringIO()
         with redirect_stdout(actual):
-            print(repr(self.fd), end='')
+            print(repr(self.fdd), end='')
         expected = "FileDict()"
         self.assertEqual(actual.getvalue(), expected)
 
     def test_str(self):
         data = [('a', 'key is a'), ('b', 'key is b')]
         for k, v in data:
-            self.fd[k] = v
+            self.fdd[k] = v
 
         actual = io.StringIO()
         with redirect_stdout(actual):
-            print(self.fd, end='')
+            print(self.fdd, end='')
         expecteds = ["FileDict(('a', 'key is a'), ('b', 'key is b'))",
                      "FileDict(('b', 'key is b'), ('a', 'key is a'))"]
         self.assertIn(actual.getvalue(), expecteds)
@@ -288,11 +315,11 @@ class TestFileDict(unittest.TestCase):
     def test_repr(self):
         data = [('a', 'key is a'), ('b', 'key is b')]
         for k, v in data:
-            self.fd[k] = v
+            self.fdd[k] = v
 
         actual = io.StringIO()
         with redirect_stdout(actual):
-            print(repr(self.fd), end='')
+            print(repr(self.fdd), end='')
         expecteds = ["FileDict(('a', 'key is a'), ('b', 'key is b'))",
                      "FileDict(('b', 'key is b'), ('a', 'key is a'))"]
         self.assertIn(actual.getvalue(), expecteds)
