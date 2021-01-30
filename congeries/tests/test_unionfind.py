1cd7246f8015e12547f27fb7713bb628b08d4c84
ReblochonMasque <trustgold@gmail.com>
2021-01-30 14:26:56 +0800

    add test_components_count_also_5

--- congeries/tests/test_unionfind.py
+++ congeries/tests/test_unionfind.py
@@ -188,13 +188,13 @@ class TestWeightedQuickUnion(unittest.TestCase):
             uf.union(p, q)
         self.assertEqual(uf.components_count, 5)
 
-    # def test_components_count_also_5(self):
-    #     uf = QuickUnion(10)
-    #     union_seq = [(4, 3), (3, 8), (6, 5), (9, 4), (2, 1), (8, 9)]
-    #     for p, q in union_seq:
-    #         uf.union(p, q)
-    #     self.assertEqual(uf.components_count, 5)
-    #
+    def test_components_count_also_5(self):
+        uf = WeightedQuickUnion(10)
+        union_seq = [(4, 3), (3, 8), (6, 5), (9, 4), (2, 1), (8, 9)]
+        for p, q in union_seq:
+            uf.union(p, q)
+        self.assertEqual(uf.components_count, 5)
+
     # def test_components_count_2(self):
     #     uf = QuickUnion(10)
     #     union_seq = [(4, 3), (3, 8), (6, 5), (9, 4), (2, 1), (8, 9),
