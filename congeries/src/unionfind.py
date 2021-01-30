786602697c9946a34ed10225ae61f6a398db4930
ReblochonMasque <trustgold@gmail.com>
2021-01-30 10:12:23 +0800

    add to the specification of the problem

--- congeries/src/unionfind.py
+++ congeries/src/unionfind.py
@@ -1,6 +1,6 @@
 """
 
-Data structures for Dynamic connectivity
+Data structures for Dynamic connectivity - Connected Components
 
 The input is a sequence of pairs of integers, where each integer represents an object
 of some type and we are to interpret the pair p q as meaning “p is connected to q.”
@@ -19,6 +19,17 @@ to read in the next pair.
 
 a data structure that can remember sufﬁcient information about the pairs it has seen to
 be able to decide whether or not a new pair of objects is connected
+
+This is useful in Network Connectivity, Variable-name equivalence, Percolation, etc.
+
+On a more abstract level, you can think of the integers as belonging to mathematical
+sets. When we process a pair p q, we are asking whether they belong to the same set. If
+not, we unite p’s set and q’s set, putting them in the same set.
+
+we will use networking terminology, and refer to the objects as sites, the pairs as
+connections, and the equivalence classes as connected components, or just components.
+
+
 
 """
 
