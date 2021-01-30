4083bc88ae0fd183a82a23e02bcf88f26ab661d0
ReblochonMasque <trustgold@gmail.com>
2021-01-31 03:51:31 +0800

    add test_elephantfiledict.py and stub TestElephantFileDict

---
+++ congeries/tests/test_elephantfiledict.py
@@ -0,0 +1,14 @@
+import unittest
+
+from congeries.src import ElephantFileDict
+from congeries.tests.test_filedict import TestFileDotDict
+
+
+class TestElephantFileDict(TestFileDotDict):
+    def test_something(self):
+        efd = ElephantFileDict('efd')
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
