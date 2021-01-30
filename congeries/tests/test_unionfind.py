a655d889bbf0575bb1e7d3edb9731cafc3c3b266
ReblochonMasque <trustgold@gmail.com>
2021-01-30 11:17:14 +0800

    comment out tests, cannot instantiate an abstract class

--- congeries/tests/test_unionfind.py
+++ congeries/tests/test_unionfind.py
@@ -2,16 +2,16 @@ import unittest
 
 from congeries.src import UnionFind
 
-
-class TestUnionFind(unittest.TestCase):
-
-    def test_type(self):
-        uf = UnionFind(10)
-        self.assertIsInstance(uf, UnionFind)
-
-    def test_array_init(self):
-        uf = UnionFind(10)
-        self.assertEqual(uf.sites, list(range(10)))
+#
+# class TestUnionFind(unittest.TestCase):
+#
+#     def test_type(self):
+#         uf = UnionFind(10)
+#         self.assertIsInstance(uf, UnionFind)
+#
+#     def test_array_init(self):
+#         uf = UnionFind(10)
+#         self.assertEqual(uf.sites, list(range(10)))
 
 
 if __name__ == '__main__':
