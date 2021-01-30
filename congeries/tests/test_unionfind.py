5a705bc8c38a8a4fdf3843bca5ad18bed22625e0
ReblochonMasque <trustgold@gmail.com>
2021-01-30 10:44:25 +0800

    add test_array_init

--- congeries/tests/test_unionfind.py
+++ congeries/tests/test_unionfind.py
@@ -9,6 +9,10 @@ class TestUnionFind(unittest.TestCase):
         uf = UnionFind(10)
         self.assertIsInstance(uf, UnionFind)
 
+    def test_array_init(self):
+        uf = UnionFind(10)
+        self.assertEqual(uf.sites, list(range(10)))
+
 
 if __name__ == '__main__':
     unittest.main()
