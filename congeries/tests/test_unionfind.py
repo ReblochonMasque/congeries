df6009431d3a61b23a5fe729225d1bb42e5816ce
ReblochonMasque <trustgold@gmail.com>
2021-01-30 14:24:25 +0800

    add test_type

--- congeries/tests/test_unionfind.py
+++ congeries/tests/test_unionfind.py
@@ -2,6 +2,7 @@ import unittest
 
 from congeries.src import QuickFind
 from congeries.src import QuickUnion
+from congeries.src import WeightedQuickUnion
 
 
 class TestQuickFind(unittest.TestCase):
@@ -140,6 +141,67 @@ class TestQuickUnion(unittest.TestCase):
         for p, q in union_seq:
             uf.union(p, q)
         self.assertEqual(uf.components_count, 2)
+
+
+class TestWeightedQuickUnion(unittest.TestCase):
+
+    def test_type(self):
+        uf = WeightedQuickUnion(10)
+        self.assertIsInstance(uf, QuickUnion)
+
+    # def test_components_count_10(self):
+    #     uf = QuickUnion(10)
+    #     self.assertEqual(uf.components_count, 10)
+    #
+    # def test_union(self):
+    #     uf = QuickUnion(10)
+    #     union_seq = [(4, 3), (3, 8), (6, 5), (9, 4), (2, 1), (8, 9),
+    #                  (5, 0), (7, 2), (6, 1), (1, 0), (6, 7)]
+    #     expected_ids = [
+    #         [0, 1, 2, 3, 3, 5, 6, 7, 8, 9],
+    #         [0, 1, 2, 8, 3, 5, 6, 7, 8, 9],
+    #         [0, 1, 2, 8, 3, 5, 5, 7, 8, 9],
+    #         [0, 1, 2, 8, 3, 5, 5, 7, 8, 8],
+    #         [0, 1, 1, 8, 3, 5, 5, 7, 8, 8],
+    #         [0, 1, 1, 8, 3, 5, 5, 7, 8, 8],
+    #         [0, 1, 1, 8, 3, 0, 5, 7, 8, 8],
+    #         [0, 1, 1, 8, 3, 0, 5, 1, 8, 8],
+    #         [1, 1, 1, 8, 3, 0, 5, 1, 8, 8],
+    #         [1, 1, 1, 8, 3, 0, 5, 1, 8, 8],
+    #         [1, 1, 1, 8, 3, 0, 5, 1, 8, 8],
+    #     ]
+    #     for (p, q), expected in zip(union_seq, expected_ids):
+    #         uf.union(p, q)
+    #         self.assertEqual(uf.id, expected)
+    #
+    # def test_components_count_7(self):
+    #     uf = QuickUnion(10)
+    #     union_seq = [(4, 3), (3, 8), (6, 5)]
+    #     for p, q in union_seq:
+    #         uf.union(p, q)
+    #     self.assertEqual(uf.components_count, 7)
+    #
+    # def test_components_count_5(self):
+    #     uf = QuickUnion(10)
+    #     union_seq = [(4, 3), (3, 8), (6, 5), (9, 4), (2, 1)]
+    #     for p, q in union_seq:
+    #         uf.union(p, q)
+    #     self.assertEqual(uf.components_count, 5)
+    #
+    # def test_components_count_also_5(self):
+    #     uf = QuickUnion(10)
+    #     union_seq = [(4, 3), (3, 8), (6, 5), (9, 4), (2, 1), (8, 9)]
+    #     for p, q in union_seq:
+    #         uf.union(p, q)
+    #     self.assertEqual(uf.components_count, 5)
+    #
+    # def test_components_count_2(self):
+    #     uf = QuickUnion(10)
+    #     union_seq = [(4, 3), (3, 8), (6, 5), (9, 4), (2, 1), (8, 9),
+    #                  (5, 0), (7, 2), (6, 1), (1, 0), (6, 7)]
+    #     for p, q in union_seq:
+    #         uf.union(p, q)
+    #     self.assertEqual(uf.components_count, 2)
 
 
 if __name__ == '__main__':
