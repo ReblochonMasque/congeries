22d9650872b0959899dc06b1bbcebefe570156d0
ReblochonMasque <trustgold@gmail.com>
2021-01-30 11:33:12 +0800

    uncomment tests - replace UnionFind with QuickFind and sites with id

--- congeries/tests/test_unionfind.py
+++ congeries/tests/test_unionfind.py
@@ -2,16 +2,16 @@ import unittest
 
 from congeries.src import QuickFind
 
-#
-# class TestUnionFind(unittest.TestCase):
-#
-#     def test_type(self):
-#         uf = UnionFind(10)
-#         self.assertIsInstance(uf, UnionFind)
-#
-#     def test_array_init(self):
-#         uf = UnionFind(10)
-#         self.assertEqual(uf.sites, list(range(10)))
+
+class TestUnionFind(unittest.TestCase):
+
+    def test_type(self):
+        uf = QuickFind(10)
+        self.assertIsInstance(uf, QuickFind)
+
+    def test_array_init(self):
+        uf = QuickFind(10)
+        self.assertEqual(uf.id, list(range(10)))
 
 
 if __name__ == '__main__':
